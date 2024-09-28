b43c8e88b110cfe7d589a228eb8dde948cc9972f
itssisi <e0969060@u.nus.edu>
2024-09-28 17:05:31 +0800

    Update mailtime.py

--- routes/mailtime.py
+++ routes/mailtime.py
@@ -6,49 +6,12 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-def calculate_working_time(start_time, end_time, user):
-    tz = pytz.timezone(user['officeHours']['timeZone'])
-    local_start = start_time.astimezone(tz)
-    local_end = end_time.astimezone(tz)
-
-    total_seconds = 0
-    work_start = user['officeHours']['start']
-    work_end = user['officeHours']['end']
-
-    while local_start < local_end:
-        # Move to the next working day if outside working hours
-        if local_start.hour >= work_end:  # If current time is after working hours
-            local_start += timedelta(days=1)
-            local_start = local_start.replace(hour=work_start, minute=0, second=0, microsecond=0)
-        elif local_start.hour < work_start:  # If current time is before working hours
-            local_start = local_start.replace(hour=work_start, minute=0, second=0, microsecond=0)
-
-        # Skip weekends
-        while local_start.weekday() >= 5:  # 5 = Saturday, 6 = Sunday
-            local_start += timedelta(days=1)
-            local_start = local_start.replace(hour=work_start, minute=0, second=0, microsecond=0)
-
-        # Calculate time until the end of the working day or the response time
-        next_work_end = local_start.replace(hour=work_end, minute=0, second=0, microsecond=0)
-        
-        if local_end < next_work_end:  # If the end time is before the workday ends
-            total_seconds += (local_end - local_start).total_seconds()
-            break
-        else:  # If the end time is after the workday ends, add time until the end of the working day
-            total_seconds += (next_work_end - local_start).total_seconds()
-            local_start = next_work_end  # Move to the end of the working day
-
-    return total_seconds
-
-def calculate_response_time(email1, email2, users):
-    sender1 = next(u for u in users if u['name'] == email1['sender'])
-    receiver1 = next(u for u in users if u['name'] == email1['receiver'])
-    
+def calculate_response_time(email1, email2):
     sent_time1 = datetime.fromisoformat(email1['timeSent']).replace(tzinfo=pytz.UTC)
     sent_time2 = datetime.fromisoformat(email2['timeSent']).replace(tzinfo=pytz.UTC)
-
-    # Calculate the time within working hours for the receiver of email2
-    return calculate_working_time(sent_time1, sent_time2, receiver1)
+    
+    # Calculate the total seconds between the two timestamps
+    return int((sent_time2 - sent_time1).total_seconds())
 
 @app.route('/mailtime', methods=['POST'])
 def mailtime():
@@ -67,7 +30,7 @@ def mailtime():
         
         # Calculate response time for the sender of email2
         user_name = email2['sender']
-        response_time = calculate_response_time(email1, email2, users)
+        response_time = calculate_response_time(email1, email2)
         
         if user_name in response_times:
             response_times[user_name].append(response_time)
