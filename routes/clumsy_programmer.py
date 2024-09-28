60f1bf949e8a0563af1d494512fb4dad9f06c84c
itssisi <e0969060@u.nus.edu>
2024-09-28 15:49:07 +0800

    Update clumsy_programmer.py

--- routes/clumsy_programmer.py
+++ routes/clumsy_programmer.py
@@ -5,13 +5,22 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
+def build_word_variants(dictionary):
+    variant_map = defaultdict(list)
+    for word in dictionary:
+        for i in range(len(word)):
+            # Create a variant by replacing the character at index i with a wildcard '*'
+            variant = word[:i] + '*' + word[i+1:]
+            variant_map[variant].append(word)
+    return variant_map
 
-def find_correct_word(mistyped_word, dictionary):
-    for correct_word in dictionary:
-        # Count how many characters differ
-        differences = sum(1 for i in range(len(mistyped_word)) if mistyped_word[i] != correct_word[i])
-        if differences == 1:
-            return correct_word
+def find_correct_word(mistyped_word, variant_map):
+    # Generate all possible variants of the mistyped word
+    for i in range(len(mistyped_word)):
+        variant = mistyped_word[:i] + '*' + mistyped_word[i+1:]
+        if variant in variant_map:
+            # Return the first match (since there should be exactly one correct match)
+            return variant_map[variant][0]
     return None  # Shouldn't happen if the input guarantees a match
 
 @app.route('/the-clumsy-programmer', methods=['POST'])
@@ -22,11 +31,13 @@ def correct_mistypes():
     for entry in data: 
         dictionary = entry.get('dictionary', [])
 
+        variant_map = build_word_variants(dictionary)
+
         mistypes = entry.get('mistypes', [])
 
         corrections = []
         for mistyped_word in mistypes:
-            corrected_word = find_correct_word(mistyped_word, dictionary)
+            corrected_word = find_correct_word(mistyped_word, variant_map)
             if corrected_word:
                 corrections.append(corrected_word)
 
